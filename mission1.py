395e43942ef0b8ec30f1f6796f08f640f9ae2c07
Asude Duman <55000545+elsmoke34@users.noreply.github.com>
2023-01-27 17:51:04 +0300

    Update mission1.py

--- mission1.py
+++ mission1.py
@@ -1,14 +1,21 @@
 
 import rospy
+from ros_essentials_cpp.msg import MyVelocity
 from turtlesim.msg import Pose
 
-def sub_callback(data):
-    rospy.loginfo(data)
+def pose_callback(pose):
+    cmd = MyVelocity()
+    cmd.vel_x = pose.linear_velocity
+    cmd.vel_y = pose.linear_velocity
+    cmd.ang_z = pose.theta
+    pub.publish(cmd)
+    print("X Doğrultusunda Lİneer Hizi:",cmd.vel_x,'\n',"Y Doğrultusunda Lİneer Hizi:",cmd.vel_y,'\n',"Baktiği Yön:", cmd.ang_z)
 
 
 if __name__ == '__main__':
-    rospy.init_node("deneme_sub")
-
-    sub = rospy.Subscriber("/turtle1/pose", Pose , callback=sub_callback)
+    rospy.init_node('mission1_node')
+    pub = rospy.Publisher('mission1_topic', MyVelocity , queue_size=10)
+    sub = rospy.Subscriber("/turtle1/pose", Pose , callback=pose_callback)
+    rospy.loginfo("Node has been started.")
 
     rospy.spin()
